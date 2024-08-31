cc5c54e6a48d239209a4777cac497dac813eddda
R.PRABHAKARA ARJUN <arjunprabhakar1910@gmail.com>
2024-08-31 14:13:56 +0530

    Time: 7 ms (19.31%), Space: 10 MB (85.47%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.c
+++ 0048-rotate-image/0048-rotate-image.c
@@ -8,28 +8,24 @@ void display(int** matrix, int matrixSize, int* matrixColSize) {
     printf("\n");
 }
 void rotate(int** matrix, int matrixSize, int* matrixColSize) {
-    //display(matrix,matrixSize,matrixColSize);
-    int **d=(int**)malloc(matrixSize*sizeof(int*));
+    display(matrix,matrixSize,matrixColSize);
     for(int i=0;i<matrixSize;i++){
-        d[i]=(int*)malloc(*matrixColSize*sizeof(int));
+        for(int j=i+1;j<*matrixColSize;j++){
+            //printf("%d %d\n",i,j);
+            int temp=matrix[i][j];
+            matrix[i][j]=matrix[j][i];
+            matrix[j][i]=temp;
+        }
+        //printf("\n");
     }
-    //new duplicated matrix!
+    //printf("\n");
     for(int i=0;i<matrixSize;i++){
-        for(int j=0;j<*matrixColSize;j++){
-            d[i][j]=matrix[i][j];
+        for(int j=0;j<*matrixColSize/2;j++){
+            int temp=matrix[i][j];
+            matrix[i][j]=matrix[i][*matrixColSize-1-j];
+            matrix[i][*matrixColSize-1-j]=temp;
         }
     }
-    //display(d,matrixSize,matrixColSize);
-    for(int i=0;i<matrixSize;i++){
-        for(int j=0;j<*matrixColSize;j++){
-            d[j][*matrixColSize-1-i]=matrix[i][j];
-        }
-    }
-    display(d,matrixSize,matrixColSize);
-    for(int i=0;i<matrixSize;i++){
-        for(int j=0;j<*matrixColSize;j++){
-            matrix[i][j]=d[i][j];
-        }
-    }
+    display(matrix,matrixSize,matrixColSize);
     
 }
