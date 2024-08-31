85b81c21e0e46f1b664f6527148a8ad559e71a17
R.PRABHAKARA ARJUN <arjunprabhakar1910@gmail.com>
2024-08-31 14:43:42 +0530

    Time: 2 ms (94.44%), Space: 46.5 MB (70.01%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] arr=new int[n+1];
+        for(int i=0;i<=n;i++){
+            arr[i]=arr[i>>1]+i%2;
+        }
+        return arr;
+    }
+}
