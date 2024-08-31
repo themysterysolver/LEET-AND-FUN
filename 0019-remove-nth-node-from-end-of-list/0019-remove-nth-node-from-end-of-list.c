b980ff480703e3d11ff1aa2ef372b772b4f4503d
R.PRABHAKARA ARJUN <arjunprabhakar1910@gmail.com>
2024-08-31 14:35:37 +0530

    Time: 6 ms (5.99%), Space: 9.2 MB (77.3%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.c
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.c
@@ -6,37 +6,32 @@
  * };
  */
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
-   struct ListNode* slow=head;
-   struct ListNode* fast=head;
+   struct ListNode* current=head;
+   int count=0;
+   while(current!=NULL){
+    count++;
+    current=current->next;
+   }
+   printf("%d\n",count);
+   int del=count-n+1;
+   printf("%d\n",del);
+   current=head;
+   del--;
    struct ListNode* prev=NULL;
-   int count=1;
-   if (n == 1) {
-        if (head->next == NULL) {
-            free(head);
-            return NULL;
-        }
-    }
-    while(count<n){
-            fast=fast->next;
-            count++;
-    }
-    if(fast->next==NULL){
+   if(del==0){
         struct ListNode* temp=head;
         head=head->next;
         free(temp);
         return head;
+   }
+    while(del!=0){
+         prev=current;
+         current=current->next;
+         del--;
     }
-    //printf("%d\n",fast->val);
-    while(fast->next!=NULL){
-        prev=slow;
-        slow=slow->next;
-        fast=fast->next;
-    }
-    //printf("%d\n",prev->val);
-    if(prev!=NULL){
-        prev->next=slow->next;
-    }
-    free(slow);
+    printf("C.VALUE:%d\n",current->val);
+    printf("PREV.VALUE:%d",prev->val);
+    prev->next=current->next;
+    free(current);
     return head;
-
 }
