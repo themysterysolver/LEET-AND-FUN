f42fa53c864ca443d82e5e864be5191535510514
R.PRABHAKARA ARJUN <arjunprabhakar1910@gmail.com>
2024-08-31 15:27:10 +0530

    Time: 67 ms (6.57%), Space: 11.7 MB (20.16%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        l=list(s)
+        g=list(goal)
+        n=len(s)
+        print l
+        while(n!=0):
+            l.append(l.pop(0))
+            print l
+            if l==g:
+                return True
+            n-=1
+        return False
+        
