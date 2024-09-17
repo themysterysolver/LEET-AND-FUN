78c5b5023696cc71484d701ef2dcb6aa6fea9fc5
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-09-17 12:32:47 +0530

    Time: 442 ms (31.67%), Space: 19.6 MB (21.74%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -5,23 +5,18 @@ class Solution:
         row,col=len(grid),len(grid[0])
         visited=[[False]*col for _ in range(row)]
         direction=[(0,1),(1,0),(0,-1),(-1,0)]
-        def BFS(r,c):
-            perimeter=0
-            q=deque([(r,c)])
+        def DFS(r,c):
+            if r<0 or c<0 or r==row or c==col or grid[r][c]==0:
+                    return 1
+            if visited[r][c]:
+                return 0
             visited[r][c]=True
-            while q:
-                x,y=q.popleft()
-                for dx,dy in direction:
-                    nx,ny=x+dx,y+dy
-                    if nx<0 or ny<0 or nx==row or ny==col or grid[nx][ny]==0:
-                        perimeter+=1
-                    elif grid[nx][ny]==1 and not visited[nx][ny]:
-                        visited[nx][ny]=True
-                        q.append((nx,ny))
-            return perimeter
-
+            perimeter=0
+            for dx,dy in direction:
+                perimeter+=DFS(r+dx,c+dy)
+            return perimeter 
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j]==1:
-                    return BFS(i,j)
+                    return DFS(i,j)
                         
