09123815786eb9d886ecf1732b0873e19847bacf
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-09-22 23:14:44 +0530

    Time: 124 ms (20.72%), Space: 24.9 MB (31.26%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,8 +1,17 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        ans=[str(i) for i in range(1,n+1)]
-        ans.sort()
-        print(ans)
-        ans=map(int,ans)
+        ans=[]
+        def dfs(cur):
+            if cur>n:
+                return
+            ans.append(cur)
+            cur*=10
+            for i in range(10):
+                dfs(cur+i)
+        for i in range(1,10):
+            dfs(i)
         return ans
+        
+            
+
         
