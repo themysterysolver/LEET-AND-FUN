6391180102ef5f3b5cd9fd10f1732795a0241475
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-09-30 19:14:58 +0530

    FIX:Create ERROR_MADE_1.py
    continue miss

---
+++ 0394-decode-string/ERROR_MADE_1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        result=""
+        string_stack=[]
+        number_stack=[]
+        i=0
+        while i<(len(s)):
+            reverse=""
+            if str(s[i]).isnumeric():
+                j=i+1
+                num=int(s[i])
+                while j < len(s) and str(s[j]).isnumeric():
+                    num=(10*num)+int(s[j])
+                    j+=1
+                print('ans',s[j])
+                i=j
+                number_stack.append(num)
+                #continue ,else we skip the [ charachter!!!!
+            elif s[i]==']':
+                while string_stack[-1]!='[':
+                    reverse=string_stack.pop()+reverse
+                string_stack.pop()
+                string_stack.append(reverse*number_stack.pop())
+            else:
+                string_stack.append(s[i])
+            i+=1
+            print(string_stack)
+            print(number_stack)
+            print('---------------------------------')
+        while string_stack:
+            result=result+string_stack.pop(0)
+        return result
+
+                
+        
