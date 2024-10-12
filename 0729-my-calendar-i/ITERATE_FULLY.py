7fea44f3a35e83ee0ca647da23b1d1debf8aaa21
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-10-12 12:34:32 +0530

    NEW:Create ITERATE_FULLY.py

---
+++ 0729-my-calendar-i/ITERATE_FULLY.py
@@ -0,0 +1,16 @@
+#[s1,e1) [s2,e2)
+#[s,e)  [start,end)
+class MyCalendar:
+    def __init__(self):
+        self.slot=[]
+    def book(self, start: int, end: int) -> bool:
+        print(self.slot)
+        for s,e in self.slot:
+            left=0,right=len(self.sort)
+            if s<end and start<e:#s<end and start<e ..............not(e<=start or end<=s)
+                return False
+        self.slot.append((start,end))
+        return True
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
