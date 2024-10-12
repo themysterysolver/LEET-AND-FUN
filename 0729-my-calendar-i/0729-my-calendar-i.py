b143e67802032a8c90729974027b57c08a80d222
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-10-12 12:43:21 +0530

    Time: 163 ms (83.69%), Space: 17.5 MB (18.9%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -4,12 +4,20 @@ class MyCalendar:
     def __init__(self):
         self.slot=[]
     def book(self, start: int, end: int) -> bool:
-        #print(self.slot)
-        for s,e in self.slot:
-            if s<end and start<e:#s<end and start<e ..............not(e<=start or end<=s)
-                return False
-        self.slot.append((start,end))
+        left,right=0,len(self.slot)-1
+        while left<=right:
+            mid=(left+right)//2
+            if self.slot[mid][0]<start:
+                left=mid+1
+            else:
+                right=mid-1
+        if left>0 and self.slot[left-1][1]>start:
+            return False
+        if left<len(self.slot) and self.slot[left][0]<end:
+            return False
+        self.slot.insert(left,(start,end))
         return True
+
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
 # param_1 = obj.book(start,end)
