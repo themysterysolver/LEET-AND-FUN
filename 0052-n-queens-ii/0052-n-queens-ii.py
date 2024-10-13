c0e22c3fc1fe3fe5291679b6eeae6deff91518c2
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-10-13 06:35:17 +0530

    Time: 44 ms (70.76%), Space: 16.8 MB (11.29%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        state=[['.']*n for _ in range(n)]
+        res=[]
+        visited_col=set()
+        visited_da=set()#diagonal
+        visited_ad=set()#antidiagonal
+        def backtrack(r):
+            if r==n:
+                res.append([''.join(row) for row in state])
+                return 
+            for c in range(n):
+                diff=r-c
+                sumi=r+c
+                if not(c in visited_col or diff in visited_da or sumi in visited_ad):       
+                    visited_col.add(c)
+                    visited_ad.add(sumi)
+                    visited_da.add(diff)
+                    state[r][c]='Q'
+                    backtrack(r+1)
+
+                    visited_col.remove(c)
+                    visited_ad.remove(sumi)
+                    visited_da.remove(diff)
+                    state[r][c]='.'
+        backtrack(0)
+        return len(res)
