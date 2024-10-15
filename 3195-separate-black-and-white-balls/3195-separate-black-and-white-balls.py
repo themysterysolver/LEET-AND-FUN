7de9aee2759369a975aba70a90bb17383124fd2b
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-10-15 06:41:51 +0530

    Time: 106 ms (49.25%), Space: 17.8 MB (20.9%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
@@ -0,0 +1,15 @@
+class Solution:                                  #Black-1 , White-0......Black to right and white to left!
+    def minimumSteps(self, s: str) -> int:
+        if '1' not in s:
+            return 0
+        if '0' not in s:
+            return 0
+        count=0
+        prev_no_of_zeroes=0
+        for i in range(len(s)-1,-1,-1):
+            if s[i]=='1':
+                count+=prev_no_of_zeroes
+            elif s[i]=='0':
+                prev_no_of_zeroes+=1
+        return count
+        
