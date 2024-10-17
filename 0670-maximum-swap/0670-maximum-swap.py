27907794dfda6e55aea1d7620c4a319485163dca
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-10-17 06:53:31 +0530

    Time: 29 ms (88.62%), Space: 16.5 MB (79.76%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -2,10 +2,6 @@ class Solution:
     def maximumSwap(self, num: int) -> int:
         nums=list(map(int,str(num)))
         #print(nums)
-        b=nums[:]
-        b.sort(reverse=True)
-        if b[:]==nums[:]:
-            return num
         max_pos=[0]*len(nums)
         max_pos[-1]=len(nums)-1
         max_pt=len(nums)-1
@@ -15,11 +11,13 @@ class Solution:
                 max_pt=i
             else:
                 max_pos[i]=max_pt
-        print(max_pos)
+        #print(max_pos)
+
         for i in range(len(nums)):
             if nums[i]>=nums[max_pos[i]]:
                 continue
             else:
                 nums[i],nums[max_pos[i]]=nums[max_pos[i]],nums[i]
                 break
+
         return int(''.join(map(str,nums)))
