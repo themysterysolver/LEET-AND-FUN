d956680a7bdab492bd00728681e1a1fc9d596eae
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-10-18 20:18:02 +0530

    Create OLD_RECURSION.py

---
+++ 0078-subsets/OLD_RECURSION.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        o=[]
+        def helper(nums,subset):
+            if not nums:
+                o.append(subset[:])
+                return
+            helper(nums[1:],subset+[nums[0]])
+            helper(nums[1:],subset)
+        helper(nums,[])
+        return o
