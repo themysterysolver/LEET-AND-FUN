662b59786ad4effe5a2fcccc88e5040fc8dd2df0
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-11-01 10:45:00 +0530

    Time: 1 ms (62.37%), Space: 16.6 MB (29.86%) - LeetHub

--- 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.py
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.py
@@ -1,17 +1,12 @@
 class Solution:
     def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
-        maxi=0
-        hash=dict()
-        for row in dimensions:
-            l,h=row[0],row[1]
-            hash[math.sqrt(l**2+h**2)]=row
-            maxi=max(maxi,math.sqrt(l**2+h**2))
-        maxy=0
-        print(maxi)
-        if maxi==65.0:
-            return 2028
-        for key,val in hash.items():
-            print(key,val,maxy)
-            if maxi==key:
-                maxy=max(maxy,val[0]*val[1])
-        return maxy
+        maxd,maxa=0,0
+        for l,h in dimensions:
+            area=l*h
+            d=l**2+h**2
+            if d>maxd:
+                maxd=d
+                maxa=area
+            elif d==maxd:
+                maxa=max(maxa,area)
+        return maxa
