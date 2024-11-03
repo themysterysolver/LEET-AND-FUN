1e6e4f91ca211e428cd587673e0a4f63fc680cc1
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-11-03 18:25:02 +0530

    Create SINGLE-NUMBER-USING-HASH.py

---
+++ 0136-single-number/SINGLE-NUMBER-USING-HASH.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d=dict()
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]=d[i]+1
+        for i in d:
+            if d[i]==1:
+                return i
