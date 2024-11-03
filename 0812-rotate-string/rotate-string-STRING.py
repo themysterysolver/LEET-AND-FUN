519f91da94d2c1d3e49ab7e7933e92d55d31e531
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-11-03 05:55:36 +0530

    Update rotate-string-STRING.py

--- 0812-rotate-string/rotate-string-STRING.py
+++ 0812-rotate-string/rotate-string-STRING.py
@@ -1,1 +1,11 @@
-
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        for i in range(len(s)+1):
+            if s[i:]+s[:i]==goal:
+                return True
+        return False     
