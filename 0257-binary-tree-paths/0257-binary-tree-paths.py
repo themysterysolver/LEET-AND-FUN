70ea4081c4ee5fbf77fd02c5041285376cfae21f
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-11-04 17:54:43 +0530

    Time: 0 ms (100%), Space: 16.6 MB (29.46%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -10,13 +10,13 @@ class Solution:
         def dfs(root,path):
             if not root:
                 return
-            path+=str(root.val)
+            if path:
+                path+="->"+str(root.val)
+            else:
+                path+=str(root.val)
             if not root.left and not root.right:
                 result.append(path)
-            dfs(root.left,path+'->')
-            dfs(root.right,path+'->')
+            dfs(root.left,path)
+            dfs(root.right,path)
         dfs(root,"")
         return result
-            
-
-        
