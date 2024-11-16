665c8a81282bd90af71b2fbd021399676cb7ce6a
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-11-16 09:03:33 +0530

    Time: 3 ms (87.77%), Space: 16.5 MB (98.63%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.py
@@ -1,17 +1,18 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-        def checker(ans):
-            flag=True
-            for i in range(len(ans)-1):
-                if ans[i+1]-ans[i]!=1:
-                    flag=False
-                    break
-            return ans[-1] if flag else -1
-        ans=[]
-        for i in range(len(nums)-k+1):
-            ans.append(nums[i:i+k])
-        print(ans)
-        result=[]
-        for i in ans:
-            result.append(checker(i))
+        if k==1:
+            return nums
+        result=[-1]*(len(nums)-k+1)
+        counter=1#counter for consecutive_element
+        #print('i,c,n,n+1')
+        for i in range(len(nums)-1):
+            if nums[i+1]-nums[i]==1:
+                counter+=1
+            else:
+                counter=1
+            if counter>=k:
+                result[i-k+2]=nums[i+1]
+            #print(i,counter,nums[i],nums[i+1])
         return result
+            
+
