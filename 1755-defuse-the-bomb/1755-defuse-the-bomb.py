49d5d7f11cc724d2fd42e93cd08eb3e219e318d7
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-11-19 06:20:10 +0530

    Time: 40 ms (11.47%), Space: 17.1 MB (5.79%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.py
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.py
@@ -7,11 +7,11 @@ class Solution:
             sumi=0
             if k>0:
                 for j in range(i+1,i+1+k):
-                    sumi+=code[j%len(code)]
+                    sumi+=code[j%len(code)]#positive overhead
             if k<0:
                 for j in range(i-abs(k),i):
                     print(i,j)
-                    sumi+=code[(j+len(code))%len(code)]
+                    sumi+=code[(j+len(code))%len(code)]#negative overhead
                 print('----------------------')
             result.append(sumi)
         return result
