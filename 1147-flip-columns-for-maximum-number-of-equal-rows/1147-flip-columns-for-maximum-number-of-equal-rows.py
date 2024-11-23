481690a84ea38a92c38aa7f8fb9d3050e0899ba0
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-11-23 10:30:44 +0530

    Time: 71 ms (48.25%), Space: 19.3 MB (51.74%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,12 +1,7 @@
-#analyse that after fliiping to optimum we realise the columns which are similar and complementary give the right answer!
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
-        maxIdenticalRows=0
+        freq=dict()
         for row in matrix:
-            flipped=[1-x for x in row]
-            count=0
-            for compareRow in matrix:
-                if compareRow==row or compareRow==flipped:
-                    count+=1
-            maxIdenticalRows=max(maxIdenticalRows,count)
-        return maxIdenticalRows
+            pattern=''.join('T' if i==row[0] else 'F' for i in row)
+            freq[pattern]=freq.get(pattern,0)+1
+        return max(freq.values())
