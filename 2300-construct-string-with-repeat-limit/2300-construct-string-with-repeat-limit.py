f303c07509b31c896270365d5c0242996814a548
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-12-17 12:24:08 +0530

    Time: 257 ms (42.39%), Space: 18.9 MB (50%) - LeetHub

--- 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.py
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.py
@@ -1,20 +1,18 @@
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
-        max_heap = [(-ord(c), cnt) for c, cnt in Counter(s).items()]
-        heapify(max_heap)
-        result = []
-
-        while max_heap:
-            char_neg, count = heappop(max_heap)
-            char = chr(-char_neg)
-            use = min(count, repeatLimit)
-            result.append(char * use)
-
-            if count > use and max_heap:
-                next_char_neg, next_count = heappop(max_heap)
-                result.append(chr(-next_char_neg))
-                if next_count > 1:
-                    heappush(max_heap, (next_char_neg, next_count - 1))
-                heappush(max_heap, (char_neg, count - use))
-
-        return "".join(result)
+        heap=[(-ord(key),val) for key,val in Counter(s).items()]
+        print(heap)
+        heapq.heapify(heap)
+        print(heap)
+        result=""
+        while heap:
+            c,val=heapq.heappop(heap)
+            c=chr(-c)
+            result+=c*min(val,repeatLimit)
+            if val>repeatLimit and heap:
+                c1,val1=heapq.heappop(heap)
+                result+=chr(-c1)
+                if val1>1:
+                    heapq.heappush(heap,(c1,val1-1))
+                heapq.heappush(heap,(-ord(c),val-min(val,repeatLimit)))
+        return result
