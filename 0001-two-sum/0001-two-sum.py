3ec275fcd0903abadad7af9cd08b5d758df6c0d7
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2024-12-26 16:32:08 +0530

    Time: 0 ms (100%), Space: 18.8 MB (8.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,20 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        
-    """
-        sumi=0
-        for i in range(0,len(nums)):
-            sumi=0
-            b=[]
-            sumi=sumi+nums[i]
-            b.append(i)
-            for j in range(i+1,len(nums)):
-                if(sumi+nums[j]==target):
-                    b.append(j)
-                    return b
-        
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash=dict()
+        for i in range(len(nums)):
+            complement=target-nums[i]
+            if complement in hash:
+                return [i,hash[complement]]
+            hash[nums[i]]=i
+        return []
+            
