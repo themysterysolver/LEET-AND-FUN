8e55a718394783cc460be44d75537158b6023f97
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2025-03-29 19:17:38 +0530

    Time: 1242 ms (71.12%), Space: 56.2 MB (85.44%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -3,17 +3,10 @@ class Solution:
         if n<=2:
             return 0
         l=[True]*n   
-        l[0]=False
-        l[1]=False
-        count=0
-        i=2
-        while (i*i)<n:
+        l[0]=l[1]=False
+        for i in range(2,int(n**0.5)+1):
             if l[i]:
-                for j in range(2*i,n,i):
+                for j in range(i*i,n,i):
                     l[j]=False
             i=i+1
-        for i in l:
-            if i:
-                count=count+1
-       
-        return  count
+        return sum(l)
