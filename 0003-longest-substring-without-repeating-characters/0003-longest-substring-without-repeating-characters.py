b80b3e7cfd91c5e66db39d77cb031f6d73e74cfc
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2025-04-25 06:25:06 +0530

    Time: 15 ms (81.94%), Space: 18 MB (12.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,16 +1,13 @@
-# d[s[i]] check only characters which are within the window ,which are less than the start doesn't matter!
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        d=dict()
-        if not s:
+        if s=="":
             return 0
-        if not s.strip() or len(s)==1:
-            return 1
-        maxi=0
+        idxHash=dict()
+        maxi=1
         start=0
         for i in range(len(s)):
-            if s[i] in d and d[s[i]]>=start: 
-                start=d[s[i]]+1
-            d[s[i]]=i
+            if s[i] in idxHash and idxHash[s[i]]>=start:
+                start=idxHash[s[i]]+1
+            idxHash[s[i]]=i
             maxi=max(maxi,i-start+1)
         return maxi
