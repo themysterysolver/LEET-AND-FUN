8744d9a0f9d3eb7b3b4564e7f16b10babf8e38d7
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2025-04-26 08:11:39 +0530

    Time: 0 ms (100%), Space: 17.8 MB (74.72%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,25 +1,13 @@
-class Solution(object):
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        if n==0:
-            print n
-            return 1
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
         if n==1:
-            print x
             return x
+        if n==0:
+            return 1
         if n<0:
             return 1/self.myPow(x,-n)
-        half=self.myPow(x,n/2)
-        print half,half*half
+        half=self.myPow(x,n//2)
         if n%2==0:
             return half*half
         else:
-            return half*x*half
-        
-            
-        
-        
+            return half*half*x
