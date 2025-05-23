3b0e9601359a5729ab69c889ddaad0ac11d4b63e
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2025-05-23 19:08:33 +0530

    Time: 92 ms (30.19%), Space: 55 MB (56.03%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.py
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.py
@@ -5,11 +5,11 @@ class Solution:
             diff[l]-=1
             diff[r+1]+=1
         currDec=0
-        print(diff)
+        #print(diff)
         for i in range(len(nums)):
             currDec+=diff[i]
             nums[i]+=currDec
-        print(nums)
+        #print(nums)
         return all(x<=0 for x in nums)
             
             
