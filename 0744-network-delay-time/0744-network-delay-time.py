db231df3a84d58b0210ca3f5268732241d4f4894
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2025-06-04 21:08:12 +0530

    Time: 368 ms (89.29%), Space: 19.8 MB (34.72%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -0,0 +1,18 @@
+#dijkstras
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adL=defaultdict(list)
+        for u,v,w in times:
+            adL[u].append((w,v))
+        spt=[float('inf')]*(n+1) #shortest path
+        spt[k]=0
+        heap=[(0,k)]
+        while heap:
+            currWeight,u=heapq.heappop(heap)
+            for weight,v in adL[u]:
+                if spt[v]>currWeight+weight:
+                    spt[v]=currWeight+weight
+                    heapq.heappush(heap,(spt[v],v))
+        #print(spt)
+        shortest_path=spt[1:]
+        return max(shortest_path) if float('inf') not in shortest_path else -1
