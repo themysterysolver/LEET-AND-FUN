7b7e9602d7cc0c036d05d58face5e2fad2c3fd13
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2025-06-07 18:05:12 +0530

    Time: 11 ms (83.3%), Space: 20.6 MB (89.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,25 +1,7 @@
-class Solution(object):
-    
-
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        def check(i):
-            di=dict()
-            for a in i:
-                if a not in di:
-                    di[a]=1
-                else:
-                    di[a]=di[a]+1
-            return di
-        anagram=dict()
-        for i in strs:
-            s=tuple(sorted(check(i).items()))
-            print s
-            if s not in anagram:
-                anagram[s]=[i]
-            else:
-                anagram[s].append(i)
-        return list(anagram.values())
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash=defaultdict(list)
+        for c in strs:
+            d=''.join(sorted(c))
+            hash[d].append(c)
+        return list(hash.values())
