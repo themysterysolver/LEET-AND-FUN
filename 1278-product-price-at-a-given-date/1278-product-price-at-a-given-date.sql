1bfc0e81b02c842ba30bea7a39ba1014a5aa0d89
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2025-07-29 19:34:37 +0530

    Time: 676 ms (19.51%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,25 +1,9 @@
 # Write your MySQL query statement below
-with before_it as (
-    select * from products p1 where change_date in
-    (select max(change_date) from products 
-    where p1.product_id = product_id and change_date <= '2019-08-16' 
-    group by product_id)
-),
-perfect as (
-select product_id,new_price as price from products where change_date = '2019-08-16' 
-),
-before_not_perfect as (
-    select product_id,new_price as price from before_it 
-    where product_id not in 
-    (select product_id from perfect)
-), def_10 as (
-    select product_id,10 as price from 
-    (select distinct product_id from products) p 
-    where p.product_id not in (select product_id from perfect)
-    and p.product_id not in (select product_id from before_not_perfect)
-)  
-select * from def_10
-union all
-select * from perfect
-union all
-select * from before_not_perfect
+select product_id,coalesce(
+    (
+        select new_price from products p
+        where p.product_id = t.product_id and p.change_date<='2019-08-16'
+        order by p.change_date desc 
+        limit 1
+    ),10
+) as price from (select distinct product_id from products) t
