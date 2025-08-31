a01313997252a7376e41d75c6df4789628f0400b
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2025-08-31 07:07:10 +0530

    Time: 1920 ms (49.58%), Space: 18 MB (63.32%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -3,41 +3,51 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        row=[set() for _ in range(9)]
-        col=[set() for _ in range(9)]
-        box=[set() for _ in range(9)]
-
+        row = [set() for _ in range(9)]
+        col = [set() for _ in range(9)]
+        box = [set() for _ in range(9)]
+        def dis(msg,mat):
+            print(msg)
+            for idx,r in enumerate(mat):
+                print(idx,r)
+            print('----------------')
         for i in range(9):
             for j in range(9):
-                if board[i][j]!=".":
+                if board[i][j]!='.':
                     row[i].add(board[i][j])
                     col[j].add(board[i][j])
                     box[(i//3)*3+j//3].add(board[i][j])
-        def backtrack(i,j):
-            if i==9:
+        #print(f"{row}\n{col}\n{box}")
+        #dis("row",row)
+        #dis("col",col)
+        #dis("box",box)
+        def backtrack(r,c):
+            if r == 9:
                 return True
-            if j==9:
-                return backtrack(i+1,0)
-            if board[i][j]!=".":
-                return backtrack(i,j+1)
-            if board[i][j]==".":
-                for n in map(str,range(1,10)):
-                    bidx=(i//3)*3+j//3
-                    if n in row[i] or n in col[j] or n in box[bidx]:
-                        continue
-                    else:
-                        row[i].add(n)
-                        col[j].add(n)
-                        box[bidx].add(n)
-                        board[i][j]=n
-                        if backtrack(i,j+1):
-                            return True
-                        board[i][j]="."
-                        row[i].remove(n)
-                        col[j].remove(n)
-                        box[bidx].remove(n)
+            elif c == 9:
+                return backtrack(r+1,0)
+            else:
+                idx = (r//3)*3+c//3
+                if board[r][c]==".":
+                    for j in range(1,10):
+                        s = str(j)
+                        if s not in row[r] and s not in col[c] and s not in box[idx]:
+                            board[r][c] = s
+                            row[r].add(s)
+                            col[c].add(s)
+                            box[idx].add(s)
+                        
+                            if backtrack(r,c+1):
+                                return True
+                            board[r][c] = "."
+                            row[r].remove(s)
+                            col[c].remove(s)
+                            box[idx].remove(s)
+                else:
+                    return backtrack(r,c+1)
                 return False
         backtrack(0,0)
         return board
+
 
                     
