7e6e1499aa8ec72a9dcf325218f96e16406d1808
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2026-06-08 16:19:29 +0530

    Time: 83 ms (30.7%), Space: 31.8 MB (5.06%) - LeetHub

---
+++ 0514-freedom-trail/0514-freedom-trail.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findRotateSteps(self, ring: str, key: str) -> int:
+        n = len(ring)
+        m = len(key)
+        hash = defaultdict(list)
+        for idx,c in enumerate(ring):
+            hash[c].append(idx)
+        print(hash)
+
+        @cache
+        def go(idx,i):
+            if i == m:
+                return 0
+            if ring[idx] == key[i]:
+                return 1+go(idx,i+1)
+            mini = float('inf')
+            for next in hash[key[i]]:
+                g = go(next,i)
+                cost = min(abs(next-idx),abs(n-abs(next-idx)))
+                mini = min(cost+g,mini)
+            return mini
+        return go(0,0)
+        
+
